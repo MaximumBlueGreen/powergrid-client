8db6d6d091a289dc96d2dade0d0784f005d5d830
JohnWestwig <john.westwig@gmail.com>
2019-02-28 14:03:21 -0500

    improve puzzle selector comp

--- app/components/PuzzleSelector/index.js
+++ app/components/PuzzleSelector/index.js
@@ -6,22 +6,31 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-function PuzzleSelector({ /* activePuzzleId, */ puzzles, onPuzzleSelected }) {
+const StyledPuzzleSelectorButton = styled.button`
+  color: ${props => (props.isActive ? 'blue' : 'black')};
+`;
+
+function PuzzleSelector({ activePuzzleId, puzzles, onPuzzleSelected }) {
   return (
     <div>
       {puzzles.map(p => (
-        <button key={p.id} type="button" onClick={() => onPuzzleSelected(p.id)}>
-          {p.id}
-        </button>
+        <StyledPuzzleSelectorButton
+          isActive={p.id === activePuzzleId}
+          key={p.id}
+          type="button"
+          onClick={() => onPuzzleSelected(p.id)}
+        >
+          {p.title || `id: ${p.id}`}
+        </StyledPuzzleSelectorButton>
       ))}
     </div>
   );
 }
 
 PuzzleSelector.propTypes = {
-  // activePuzzleId: PropTypes.string.isRequired,
+  activePuzzleId: PropTypes.string.isRequired,
   puzzles: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   onPuzzleSelected: PropTypes.func.isRequired,
 };
