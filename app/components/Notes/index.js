af64e8d71511c25457419d5c2240b915d28aedc5
JohnWestwig <john.westwig@gmail.com>
2019-04-10 23:29:21 -0400

    Add entry reference styling to notes comp

--- app/components/Notes/index.js
+++ app/components/Notes/index.js
@@ -6,27 +6,90 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link, Paper } from '@material-ui/core';
+import {
+  Editor,
+  EditorState,
+  CompositeDecorator,
+  ContentState,
+} from 'draft-js';
 
-import { TextField } from '@material-ui/core';
+const EntryTag = props => (
+  <Link href="/dashboard" color="primary">
+    {props.children}
+  </Link>
+);
+EntryTag.propTypes = {
+  children: PropTypes.array,
+};
 
-function Notes({ onEdit, notes }) {
-  return (
-    <TextField
-      value={notes}
-      onChange={e => onEdit(e.target.value)}
-      multiline
-      fullWidth
-      rows="25"
-      margin="normal"
-      variant="outlined"
-      placeholder="This is your place to brainstorm theme ideas and clues and to leave notes for yourself, the editor or your test solvers."
-    />
-  );
+function entryTagStrategy(contentBlock, callback) {
+  findWithRegex(/@[\d]+(A|D|a|d)/g, contentBlock, callback);
+}
+
+function findWithRegex(regex, contentBlock, callback) {
+  const text = contentBlock.getText();
+  let matchArr = regex.exec(text);
+  let start;
+  while (matchArr !== null) {
+    start = matchArr.index;
+    callback(start, start + matchArr[0].length);
+    matchArr = regex.exec(text);
+  }
+}
+
+class Notes extends React.Component {
+  constructor(props) {
+    super(props);
+    const compositeDecorator = new CompositeDecorator([
+      {
+        strategy: entryTagStrategy,
+        component: EntryTag,
+      },
+    ]);
+
+    if (props.notes) {
+      this.state = {
+        editorState: EditorState.createWithContent(
+          ContentState.createFromText(props.notes),
+          compositeDecorator,
+        ),
+      };
+    } else {
+      this.state = {
+        editorState: EditorState.createEmpty(compositeDecorator),
+      };
+    }
+
+    this.onChange = editorState => this.setState({ editorState });
+  }
+
+  render() {
+    return (
+      <Paper
+        style={{
+          padding: '20px',
+          paddingBottom: '40px',
+          fontSize: '16px',
+          margin: '10px',
+        }}
+      >
+        <Editor
+          placeholder="Brainstorm theme ideas &amp;	clues. Leave notes for yourself, the editor or your test solvers."
+          editorState={this.state.editorState}
+          onChange={editorState => {
+            this.onChange(editorState);
+            this.props.onEdit(editorState.getCurrentContent().getPlainText());
+          }}
+        />
+      </Paper>
+    );
+  }
 }
 
 Notes.propTypes = {
-  onEdit: PropTypes.func.isRequired,
   notes: PropTypes.string,
+  onEdit: PropTypes.func.isRequired,
 };
 
 Notes.defaultProps = {
