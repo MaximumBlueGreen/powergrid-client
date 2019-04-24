98b67a59a614c6f90864643ec838736b2f1b44f1
JohnWestwig <john.westwig@gmail.com>
2019-04-24 14:49:35 -0400

    Fix login bug

--- app/entities/Users/reducer.js
+++ app/entities/Users/reducer.js
@@ -1,5 +1,5 @@
 import { fromJS } from 'immutable';
-import { SUCCESSFUL_USER_LOGIN_ACTION } from 'containers/LoginPage/constants';
+import { SUCCESSFUL_USER_LOGIN_ACTION } from 'containers/LoginContainer/constants';
 import { ENTITIES_LOADED } from 'entities/constants';
 const initialState = fromJS({ me: localStorage.getItem('token') });
 
